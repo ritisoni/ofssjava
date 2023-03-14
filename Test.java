d16f48e63f83ff97e129a3b14923536eee989074
ritisoni <127746392+ritisoni@users.noreply.github.com>
2023-03-14 11:25:22 +0530

    updated test.java

--- Test.java
+++ Test.java
@@ -3,6 +3,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi");
+		System.out.println("hi 1");
+		System.out.println("hi 2");
 	}
 
 }
